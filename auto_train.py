5cc3ef005244a6e130298583afaf0032aeaf8556
William Chung <r05723047@ntu.edu.tw>
2018-04-18 21:55:01 +0800

    roll_train

---
+++ auto_train.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from agent import Agent
+from network import CNN
+
+
+def auto_train(Agent, config):
+    training_steps = config['training_steps']
+    print_process = config['print_process']
+    early_stop_number = config['early_stop']
+    save_model = config['save_model']
+    save_step = config['save_step']
+
+    LOSS_train = []
+    LOSS_test = []
+
+    print('start trainning ...')
+    for step in range(training_steps):
+        b_S, b_y, b_I = Agent.next_batch()
+        Agent.learn(b_S,b_y,b_I)
+
+        if (step % save_step ==0) and (step!=0) :
+            loss_train = Agent.loss( Agent.memory['S'],
+                                Agent.memory['y'],
+                                Agent.memory['I'])
+
+            loss_test = Agent.loss( Agent.memory_val['S'],
+                                Agent.memory_val['y'],
+                                Agent.memory_val['I'])
+            if print_process:
+                print('step:',step,
+                    'trainning loss:', loss_train,
+                    'testing loss:', loss_test)
+
+            if len(LOSS_test)>1:
+                if LOSS_test[-1] < loss_test:
+                    early_stop += 1
+                    if early_stop == early_stop_number:
+                        print('step:',step,
+                            'trainning loss:', loss_train,
+                            'testing loss:', loss_test)
+                        
+                        print('early stop')
+                        break
+                else:
+                    early_stop = 0
+
+            LOSS_train.append(loss_train)
+            LOSS_test.append(loss_test)
+
+    return Agent, LOSS_train, LOSS_test
---
+++ roll_train.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from agent import Agent
+from config import *
+from network import CNN
+from auto_train import auto_train
+
+#tf.reset_default_graph()
+NNAgent = Agent(config)
+
+epoch = 0
+while True:
+    print('epoch: ', epoch)
+    NNAgent, LOSS_train, LOSS_test = auto_train(NNAgent, config)
+
+    # save model
+    model_name = config['index'] + '-' + str(NNAgent.network.n_assets)
+    NNAgent.save_model( model_name, epoch)
+
+    # weight
+    _w = NNAgent.weight( NNAgent.memory_val['S'] )
+    weight_index = _w[0]>1e-4
+
+    if weight_index.sum() == weight_index.shape[0]:
+        print('roll_train done')
+        break
+    else:        
+        epoch += 1
+        # reset memory
+        NNAgent.reset_Memory(weight_index)
+
+        # reset n_assets
+        assets = NNAgent.dm.assets.copy()
+        for i in range(weight_index.shape[0]):
+            if not weight_index[i]:
+                assets.remove(NNAgent.dm.assets[i])
+        NNAgent.dm.assets = assets
+        NNAgent.network.n_assets = len(NNAgent.dm.assets)
+        print('number of assets: ', NNAgent.network.n_assets)
+
+        # reset network
+        tf.reset_default_graph()
+        NNAgent.network = CNN(n_features=NNAgent.memory['S'].shape[-1],
+                         n_assets=NNAgent.memory['S'].shape[1],
+                         config=config)
